6873c0eb292f86c0e0d99500940d42d03720d4c1
TalissonSouza <talissonmdsouza@gmail.com>
2023-12-20 20:16:02 -0300

    header

--- template/header.php
+++ template/header.php
@@ -1,6 +1,10 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 
+    <?php
+    session_start();
+    session_destroy(); ?>
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
